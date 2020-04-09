53310f110676c942b29e367280747e1e8ba08681
陳宛昀 <lov011052@gmail.com>
2020-04-09 08:57:53 +0800

    練習類別、成員、資料類型與欄位(0409)

--- ClassField/Assets/Enemy.cs
+++ ClassField/Assets/Enemy.cs
@@ -8,47 +8,46 @@ public class Enemy : MonoBehaviour
     /// <summary>
     /// 怪獸的血量
     /// </summary>
-    [Header("怪獸的血量"),Tooltip("怪獸的血量"),Range(50,500)]
+    [Header("血量"),Tooltip("怪獸的血量"),Range(50,500)]
     public int HP;
     /// <summary>
     /// 怪獸的魔力
     /// </summary>
-    [Header("怪獸的魔力"),Tooltip("怪獸的魔力"),Range(10,100)]
+    [Header("魔力"),Tooltip("怪獸的魔力"),Range(10,100)]
     public int MP = 100;
     /// <summary>
     /// 怪獸的移動速度
     /// </summary>
-    [Header("怪獸的移動速度"),Tooltip("怪獸的移動速度"),Range(1,50.5f)]
+    [Header("能力值"),Range(1,50.5f)]
     public float Move_Speed = 10.5f;
     /// <summary>
     /// 怪獸的攻擊力
     /// </summary>
-    [Header("怪獸的攻擊力"), Tooltip("怪獸的攻擊力"), Range(20, 200)]
+    [Range(20, 200)]
     public int Attack = 50;
     /// <summary>
     /// 怪獸的防禦力
     /// </summary>
-    [Header("怪獸的防禦力"), Tooltip("怪獸的防禦力"), Range(20, 200)]
+    [Range(20, 200)]
     public int Defense;
     /// <summary>
     /// 怪獸的武器名稱 
     /// </summary>
-    [Header("怪獸的武器"), Tooltip("怪獸的武器名稱")]
+    [Header("裝備")]
     public string Arms;
     /// <summary>
     /// 怪獸的裝備名稱
     /// </summary>
-    [Header("怪獸的裝備"), Tooltip("怪獸的裝備名稱")]
     public string Equip = "皮衣";
     /// <summary>
     /// 怪獸是否有拿鑰匙
     /// </summary>
-    [Header("怪獸是否有拿鑰匙"), Tooltip("怪獸是否有拿鑰匙")]
+    [Header("其他資料"), Tooltip("怪獸是否帶有鑰匙")]
     public bool Key = false;
     /// <summary>
     /// 怪獸是否掉落寶物
     /// </summary>
-    [Header("怪獸是否掉落寶物"), Tooltip("怪獸是否掉落寶物")]
+    [Tooltip("怪獸是否帶有寶物")]
     public bool Treasure = false;
 
 
